9d0eee328baf31becae83ba10245c95e2c61cb86
rudchyk <sergii.rudchyk@gmail.com>
2022-02-18 07:43:54 +0200

    added cart

--- ecomm/container/src/bootstrap.js
+++ ecomm/container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow';
+import { mount as productsMount } from 'products/ProductsIndex';
+import { mount as cartMount } from 'cart/CartShow';
 
-console.log('Container!');
+console.log('Container NEW!');
+
+productsMount(document.querySelector('#container-products'));
+cartMount(document.querySelector('#container-cart'));
