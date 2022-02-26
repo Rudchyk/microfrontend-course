ca96539d3369758c865ec5cd5d801c2b5c0490b8
rudchyk <sergii.rudchyk@gmail.com>
2022-02-26 13:39:47 +0200

    added nav

--- mfp/packages/marketing/src/App.js
+++ mfp/packages/marketing/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, Router } from 'react-router-dom';
 import {
   StylesProvider,
   createGenerateClassName,
@@ -12,18 +12,18 @@ const generateClassName = createGenerateClassName({
   productionPrefix: 'ma',
 });
 
-const App = () => {
+const App = ({ history }) => {
   return (
-    <BrowserRouter>
+    <div>
       <StylesProvider generateClassName={generateClassName}>
-        <div>
+        <Router history={history}>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
             <Route path="/" component={Landing} />
           </Switch>
-        </div>
+        </Router>
       </StylesProvider>
-    </BrowserRouter>
+    </div>
   );
 };
 
