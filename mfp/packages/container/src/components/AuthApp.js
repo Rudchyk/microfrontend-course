32db8672989104f4088d1f75cd4ef1c9dc41c0f4
rudchyk <sergii.rudchyk@gmail.com>
2022-02-26 15:50:44 +0200

    auth set

--- mfp/packages/container/src/components/AuthApp.js
+++ mfp/packages/container/src/components/AuthApp.js
@@ -1,4 +1,4 @@
-import { mount } from 'marketing/MarketingApp';
+import { mount } from 'auth/AuthApp';
 import React, { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
@@ -9,8 +9,7 @@ export default () => {
   useEffect(() => {
     const { onParentNavigate } = mount(ref.current, {
       initialPath: history.location.pathname,
-      onNavigate: (location) => {
-        const { pathname: nextPathname } = location;
+      onNavigate: ({ pathname: nextPathname }) => {
         const { pathname } = history.location;
 
         if (pathname !== nextPathname) {
